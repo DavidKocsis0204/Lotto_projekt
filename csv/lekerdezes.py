cfb4823d82c68b913f1e83e48f5b8dcc37c820fd
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2022-01-05 12:47:06 +0100

    python harmadik lekérdezés javítása

--- csv/lekerdezes.py
+++ csv/lekerdezes.py
@@ -1,16 +1,61 @@
 import csv
+from os import system
 
-with open('H:\Python\otos.csv', 'r') as file:
+system('cls')
+
+with open('H:\Lotto_projekt-LTP-6\csv\otos.csv', 'r') as file:
     reader = csv.reader(file)
     tomb=[]
     line=0
     for row in reader:
         if  line==0:
+            line=line+1
             continue
-        tomb.append(row)
+        else:
+            line=line+1
+            
+            tomb.append(row[0].split(';')[11:16])
+    #teszteléshez
+    #print(tomb[0])
+
 #Beolvasás--------------------------
-    sro=str(tomb[1]).split(';')
-    print(sro[11:16])
+    sor_elso=[]
+    sor_elso.append(tomb[0][0:5])
+    print(sor_elso)
 #Lekérdezés_1-----------------------
-    sro_ketto=str(tomb[2]).split(;)
-    print(sro_ketto[11:16])
+    sor_masodik=[]
+    sor_masodik.append(tomb[1][0:5])
+    print(sor_masodik)
+#lekérdezés_2-----------------------
+    db=[]
+
+    for i in range(1,91):
+        szamolo=0
+        for j in range(len(tomb)):
+            for n in range(5):
+                if int(tomb[j][n])==i:
+                    szamolo+=1
+        db.append(szamolo)
+    
+    for i in range(len(db)):
+        if db[i]==208:
+            print(f"{i+1} 208")
+        elif db[i]==209:
+            print(f"{i+1} 209")
+        elif db[i]==218:
+            print(f"{i+1} 218")
+
+    #sorba rendezés
+    #sorrend=db
+    #for i in range(len(sorrend)):
+    #    for j in range(len(sorrend)-1):
+    #        if sorrend[j]>sorrend[i]:
+    #            c=sorrend[j]
+    #            sorrend[j]=sorrend[i]
+    #            sorrend[i]=c
+#lekérdezés3---------------------------------
+
+    for i in range(len(tomb)):
+        seged_tomb=tomb[i]
+        for j in range(10):
+            
